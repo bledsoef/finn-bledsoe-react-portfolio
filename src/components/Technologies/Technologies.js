598ed8d1c1c1729c757f711249bbab29125cd043
bledsoef <bledsoef@berea.edu>
2022-07-24 17:02:59 -0400

    created basic timeline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { DiCss3, DiFirebase, DiHtml5, DiJavascript, DiPython, DiReact, DiZend } from 'react-icons/di';
+import { CgCPlusPlus } from 'react-icons/cg';
+import { SiFlask } from 'react-icons/Si';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <SectionTitle main>Technologies</SectionTitle>
+    <SectionText>
+      Insert text here
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiPython size="3rem"/>
+        <ListContainer>
+          <ListTitle>Python</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJavascript size="3rem"/>
+        <ListContainer>
+          <ListTitle>JavaScript</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiHtml5 size="3rem"/>
+        <ListContainer>
+          <ListTitle>HTML5</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiCss3 size="3rem"/>
+        <ListContainer>
+          <ListTitle>CSS</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem"/>
+        <ListContainer>
+          <ListTitle>React</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <CgCPlusPlus size="3rem"/>
+        <ListContainer>
+          <ListTitle>C++</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <SiFlask size="3rem"/>
+        <ListContainer>
+          <ListTitle>Flask</ListTitle>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiPython size="3rem"/>
+        <ListContainer>
+          <ListTitle>Python</ListTitle>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
